3862c28d32b905396c152bf24bfd3bb1d51eca21
F1lus <48390306+F1lus@users.noreply.github.com>
2021-03-06 16:56:41 +0100

    Update Helpdesk.js

--- client/src/components/home/Helpdesk.js
+++ client/src/components/home/Helpdesk.js
@@ -1,17 +1,61 @@
 export const Adminnak = [
     {
-        kerdes: "Globális statisztika",
-        valasz: "Az eddig önáltal létrehozott vizsgákról általános adatok."
+        kerdes: "Mire való ez a weboldal?",
+        valasz: "Ez a weboldal lehetőséget ad az adminisztrátoroknak a dolgozók oktatására, és vizsgáztatására."
     },
     {
-        kerdes: "Vizsgánkénti statisztika",
-        valasz: "Az eddig önáltal létrehozott."
+        kerdes: "Hogyan tudok tananyagot feltölteni?",
+        valasz: "Minden tananyaghoz tartoznia kell egy vizsgának. Amint létrehoz egy vizsgát a Vizsgák fülön, azzal kötelezően egy PDF kiterjesztésű, maximum 2 megabájt nagyságú tananyagot kell csatolnia. (A vizsga elkészítése után az automatikusan inaktív, így a dolgozók még nem férhetnek hozzá.)"
     },
+    {
+        kerdes: 'Hogyan hozok létre egy vizsgát?',
+        valasz: 'A Vizsgák fülön értelemszerűen létrehozza a vizsgát. A létrehozás után automatikusan belép a szerkesztő módba, ahol a vizsga tulajdonságaitól kezdve a kérdéseket, és a válaszokat is testreszabhatja.'
+    },
+    {
+        kerdes: 'Mit jelent az, hogy egy vizsga Aktív, vagy Inaktív?',
+        valasz: 'Aktív állapotban a vizsga kitöltése elérhető a dolgozók számára, míg Inaktív állapotban nem.'
+    },
+    {
+        kerdes: 'Később is lehet módosítani a már létrehozott vizsgákat?',
+        valasz: 'Igen, a vizsgák bármikor tetszés szerint módosíthatóak a Vizsgák fülön.'
+    },
+    {
+        kerdes: 'Ki módosíthatja az általam készített vizsgákat?',
+        valasz: 'Mivel Ön készítette a vizsgát, így csak és kizárólag Önnek van erre jogosultsága.'
+    },
+    {
+        kerdes: 'Hol találhatók a statisztikák?',
+        valasz: 'Az adminisztrátorok számára a Profil fülön elérhető a globális, és a vizsgánkénti statisztika.'
+    },
+    {
+        kerdes: 'Mi az a globális statisztika?',
+        valasz: 'A globális statisztika az Ön által készített vizsgák eredményei összesítve.'
+    },
+    {
+        kerdes: 'Mi az a vizsgánkénti statisztika?',
+        valasz: 'A vizsgánkénti statisztika akkor jön elő, ha a Profil fülön kiválasztott egy vizsgát. Ott szigorúan az adott vizsgák eredményeiről kap egy összesített értékelést.'
+    }
 ]
 
 export const Usernek = [
     {
-        kerdes: "Globális statisztika",
-        valasz: "Az eddig önáltal teljesített vizsgákról általános adatok."
+        kerdes: "Mire való ez a weboldal?",
+        valasz: "Itt különböző tananyagokat tanulhat meg egyes termékek elkészítésével kapcsolatban. A tanultakból később vizsgát kell tennie."
+    },
+    {
+        kerdes: 'Hogyan tanulhatok meg valamit?',
+        valasz: 'Ha az adott termékhez már tartozik tananyag, akkor a profil fülön találhatja meg.'
+    },
+    {
+        kerdes: 'Hogyan működik a vizsgázás?',
+        valasz: 'Az Ön számára elérhető vizsgák a Vizsgák fülön találhatóak meg. Amint belép egy vizsgába, onnantól kezdve bármilyen módú félbeszakítás/kilépés a vizsga azonnali lezárását vonja maga után. A vizsgázás alatt több válasz is megjelölhető, de ha a válaszok között van legalább egy rossz válasz, akkor az adott kérdésre nem jár pont.'
+    },
+    {
+        kerdes: 'Van lehetőség újból vizsgázni?',
+        valasz: 'A vizsgák csak egyszer elvégezhetőek, hacsak a vizsga készítője nem rendelkezik erről másképp.'
+    },
+    {
+        kerdes: 'Hol tekinthetem meg az eredményeimet?',
+        valasz: 'A Profil fülön minden eredmény megtekinthető statisztikákkal együtt.'
     }
 ]
