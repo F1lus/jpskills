6eb8c7d26932a6f80843ac1b5947547328fb25f0
F1lus <48390306+F1lus@users.noreply.github.com>
2021-03-12 16:53:26 +0100

    Fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
 export default {
     server_address: 'http://localhost:5000'
 }
+//A kliens címét, és portját a .env fájlban lehet majd módosítani
+//A későbbiekben ennek a fájlnak a tartalma is oda lesz áthelyezve
