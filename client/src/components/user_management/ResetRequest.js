4083ecdba2d6d73c64922906c416434eed0cd39d
F1lus <48390306+F1lus@users.noreply.github.com>
2021-07-08 22:34:53 +0200

    Update ResetRequest.js

--- client/src/components/user_management/ResetRequest.js
+++ client/src/components/user_management/ResetRequest.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+
 import { NavLink } from 'react-router-dom'
 
+import API from '../BackendAPI'
+
 export default function ResetRequest() {
+
+
+    const [cardNum, setCardNum] = useState(null)
+
+    const handleChange = useCallback(event => {
+        setCardNum(event.target.value)
+    }, [])
+
+    const handleSubmit = useCallback(event => {
+        event.preventDefault()
+
+    }, [])
 
     return (
         <div className="d-flex flex-column container text-center align-items-center justify-content-center vh-100 w-50">
@@ -10,9 +25,9 @@ export default function ResetRequest() {
                     <p>Jelszóvisszaállítási kérelem!</p>
                 </h2>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-group m-auto">
-                        <input type="text" name="cardNum" autoComplete="off" required />
+                        <input type="text" name="cardNum" autoComplete="off" value={cardNum || ''} onChange={handleChange} required />
                         <label htmlFor="cardNum" className="label-name">
                             <span className="content-name">
                                 Kártyaszám
