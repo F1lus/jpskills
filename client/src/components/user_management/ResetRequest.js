5757e525faa6af8862df8f00dae5b4a1b735e8d6
F1lus <48390306+F1lus@users.noreply.github.com>
2021-07-08 23:20:51 +0200

    Update ResetRequest.js

--- client/src/components/user_management/ResetRequest.js
+++ client/src/components/user_management/ResetRequest.js
@@ -8,6 +8,7 @@ export default function ResetRequest() {
 
 
     const [cardNum, setCardNum] = useState(null)
+    const [alert, setAlert] = useState(null)
 
     const handleChange = useCallback(event => {
         setCardNum(event.target.value)
@@ -16,7 +17,24 @@ export default function ResetRequest() {
     const handleSubmit = useCallback(event => {
         event.preventDefault()
 
-    }, [])
+        if(!cardNum){
+            setAlert('Kérjük ne hagyja a mezőt üresen!')
+            return
+        }
+
+        API.post('/requestReset', {cardNum: cardNum})
+            .then(result => {
+                if(result.data.response){
+                    setAlert('Új jelszó igényléséhez kérjük kattintson az emailben szereplő linkre!')
+                }else if(result.data.err){
+                    setAlert(result.data.err)
+                }else{
+                    setAlert('A kérelmét nem sikerült feldolgozni, kérjük próbálja újra később!')
+                }
+            })
+            .catch(err => console.log(err))
+
+    }, [cardNum])
 
     return (
         <div className="d-flex flex-column container text-center align-items-center justify-content-center vh-100 w-50">
@@ -24,7 +42,7 @@ export default function ResetRequest() {
                 <h2>
                     <p>Jelszóvisszaállítási kérelem!</p>
                 </h2>
-
+                {alert ? <h3 className="alert alert-danger text-center" id="hiba">{alert}</h3> : null}
                 <form onSubmit={handleSubmit}>
                     <div className="form-group m-auto">
                         <input type="text" name="cardNum" autoComplete="off" value={cardNum || ''} onChange={handleChange} required />
